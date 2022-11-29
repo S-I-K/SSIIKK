44274a80628e23c90dcffcd52349412a336f3d6d
sik <daesik99@naver.com>
2022-11-29 18:50:48 +0900

    update/ jquery use

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
-/* a 태그의 새로고침 기능 제거 */
-let a = document.querySelectorAll("a");
-console.log(a);
-
-function prevent(e) {
-  e.preventDefault();
-}
-
-for (let i = 0; i < a.length; i++) {
-  a[i].addEventListener("click", prevent);
-}
+$(() => {
+  /* header fix */
+  $(".gnb").scrollToGiveClass({
+    baseline: 90,
+    class: "fixed",
+  });
+  /* slide scroll event */
+  $(".banner").bxSlider({
+    auto: "true",
+    controls: false,
+    pause: 3000,
+  });
+  /* season item event */
+  $(".season-title-wrap").scrollToGiveClass({
+    baseline: "bottom",
+  });
+  $(".content").scrollToGiveClass({
+    baseline: "bottom",
+  });
+  $(".season-item").scrollToGiveClass({
+    baseline: "bottom",
+  });
+  /* product item scroll event */
+  $(".product-wrap .title-wrap").scrollToGiveClass({
+    baseline: "bottom",
+  });
+  $(".product-wrap label").scrollToGiveClass({
+    baseline: "bottom",
+  });
+  $(".product-wrap .content-wrap .item-box").scrollToGiveClass({
+    baseline: "bottom",
+  });
+});
